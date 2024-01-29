752df510e1cc0c4e6a28c2646f56df1cdacdc783
ogandril <Olivier.Gandrillon@ens-lyon.fr>
2024-01-29 12:43:34 +0100

    Draw

--- Draw_GRN.py
+++ Draw_GRN.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from harissa.utils import build_pos, plot_network
 from sys import argv 
 
-Time_Line=0
+Time_Line=1
 # Time_Line: draw a GRN per time point 
 
 Remove_leaves = 0
@@ -54,40 +54,13 @@ def pgr(datamatrixarray,i):
 
 os.chdir(str(cwd)+"/OG"+str(D)+"/"+str(P)+"/cardamom")
 datamatrixarray = np.load('inter.npy')
-pgr(datamatrixarray,"a")
-
+pgr(datamatrixarray,"all")
 
 
 if Time_Line:
 	os.chdir(str(cwd)+"/OG"+str(D)+"/"+str(P)+"/cardamom")
 	dim=np.load('inter_t.npy')
-	for i in range(0, 3):
-	#for i in range(0, len(dim)):		    
+	for i in range(0, len(dim)):		    
 		datamatrixarray = np.load('inter_{}.npy'.format(i))
-		pgr(datamatrixarray)
-
-
-# Get the names of the nodes
-		with open('../Data/Genenames.txt') as f:
-			Genenames = f.read().splitlines()
-			g2=np.asarray(Genenames)
-			Genenames=g2[np.ix_(z2[0])]
-
-# Node positions and names
-		pos = build_pos(datamatrixarray)
-# scale to the number of genes
-		pos *= 5
-
-# Figure
-		fig = plt.figure(figsize=(5,5))
-		ax = fig.gca()
-
-# Draw the network
-		plot_network(datamatrixarray, pos, axes=ax, names=Genenames, scale=2)
-
-# Export the figure
-		ti='GRN.OG'+str(D)+'_'+str(P)+'_'+'Threshold='+str(T)+'_Time='+i+'pdf'
-		os.chdir(str(cwd)+"/OG"+str(D)+"/"+str(P)+"/Results")
-		fig.savefig(ti, bbox_inches='tight')
-
+		pgr(datamatrixarray,i)
 
