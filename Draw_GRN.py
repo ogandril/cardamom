f4823e72b7ed41be659a9b48d3f22df89592e2d4
ogandril <Olivier.Gandrillon@ens-lyon.fr>
2024-01-29 12:24:18 +0100

    Draw

--- Draw_GRN.py
+++ Draw_GRN.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from harissa.utils import build_pos, plot_network
 from sys import argv 
 
-Time_Line=1 
+Time_Line=0
 # Time_Line: draw a GRN per time point 
 
 Remove_leaves = 0
@@ -20,44 +20,41 @@ P=argv[2]
 T=argv[3]
 cwd=argv[4]
 
-os.chdir(str(cwd)+"/OG"+str(D)+"/"+str(P)+"/cardamom")
-
-# datamatrixarray = np.loadtxt('inter_matrix.csv', delimiter=',')
-datamatrixarray = np.load('inter.npy')
-
+def pgr(datamatrixarray):
 # Sum rows
-s1=datamatrixarray.sum(axis=0)
+	s1=datamatrixarray.sum(axis=0)
 # Sum columns
-s2=datamatrixarray.sum(axis=1)
+	s2=datamatrixarray.sum(axis=1)
 # Find when both are null
-z1=np.logical_and(s1==0, s2==0)
+	z1=np.logical_and(s1==0, s2==0)
 # Get the index where both are not null
-z2=np.where(z1==0)
+	z2=np.where(z1==0)
 # Select
-datamatrixarray=datamatrixarray[np.ix_(z2[0],z2[0])]
-
+	datamatrixarray=datamatrixarray[np.ix_(z2[0],z2[0])]
 # Get the names of the nodes
-with open('../Data/Genenames.txt') as f:
-	Genenames = f.read().splitlines()
-	g2=np.asarray(Genenames)
-	Genenames=g2[np.ix_(z2[0])]
-
+	with open('../Data/Genenames.txt') as f:
+		Genenames = f.read().splitlines()
+		g2=np.asarray(Genenames)
+		Genenames=g2[np.ix_(z2[0])]
 # Node positions and names
-pos = build_pos(datamatrixarray)
+	pos = build_pos(datamatrixarray)
 # scale to the number of genes
-pos *= 5
-
+	pos *= 5
 # Figure
-fig = plt.figure(figsize=(5,5))
-ax = fig.gca()
-
+	fig = plt.figure(figsize=(5,5))
+	ax = fig.gca()
 # Draw the network
-plot_network(datamatrixarray, pos, axes=ax, names=Genenames, scale=2)
+	plot_network(datamatrixarray, pos, axes=ax, names=Genenames, scale=2)
 
+
+os.chdir(str(cwd)+"/OG"+str(D)+"/"+str(P)+"/cardamom")
+datamatrixarray = np.load('inter.npy')
+pgr(datamatrixarray)
 # Export the figure
 ti='GRN.OG'+str(D)+'_'+str(P)+'_'+'.Threshold='+str(T)+'.pdf'
 os.chdir(str(cwd)+"/OG"+str(D)+"/"+str(P)+"/Results")
 fig.savefig(ti, bbox_inches='tight')
+
 
 
 if Time_Line:
