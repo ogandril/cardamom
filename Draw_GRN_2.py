80d9a69b884bdfb1c549631a45a1af5be10d838e
ogandril <Olivier.Gandrillon@ens-lyon.fr>
2024-03-07 06:43:05 +0100

    Draw again

--- Draw_GRN_2.py
+++ Draw_GRN_2.py
@@ -24,31 +24,28 @@ def pgr(datamatrixarray,i,pos):
 	fig = plt.figure(figsize=(5,5))
 	ax = fig.gca()
 # Draw the network
-	plot_network(datamatrixarray, pos, axes=ax, names=Genenames, scale=2, hide_isolated_genes=True, hide_stimulus_leaves=True)
+	plot_network(datamatrixarray, pos, axes=ax, names=Genenames, scale=2)
 # Export the figure
 	os.chdir(str(cwd)+"/OG"+str(D)+"/"+str(P)+"/Results")
 	ti='GRN.OG'+str(D)+'_'+str(P)+'_'+'Threshold='+str(T)+'_Time='+str(i)+'.pdf'
 	fig.savefig(ti, bbox_inches='tight')
 
+# Define pos
 os.chdir(str(cwd)+"/OG"+str(D)+"/"+str(P)+"/cardamom")
 datamatrixarray = np.load('inter.npy')
 # Node positions and names
 pos = build_pos(datamatrixarray)
 # scale to the number of genes
 pos *= 8
+
+# Draw complete GRN
 pgr(datamatrixarray,"all", pos)
 
+# Draw time-dependent GRNs
 if Time_Line:
-	os.chdir(str(cwd)+"/OG"+str(D)+"/"+str(P)+"/cardamom")
-	datamatrixarray = np.load('inter.npy')
-# Node positions and names
-	pos = build_pos(datamatrixarray)
-# scale to the number of genes
-	pos *= 8
 	dim=np.load('inter_t.npy')
-	for i in range(0, len(dim)):	
-		os.chdir(str(cwd)+"/OG"+str(D)+"/"+str(P)+"/cardamom")	    
-		datamatrixarray = np.load('inter_{}.npy'.format(i))
-		pgr(datamatrixarray,i,pos)
+	for i in range(0, len(dim)):	   
+		dmt = np.load('inter_{}.npy'.format(i))
+		pgr(dmt,i,pos)
 
 
