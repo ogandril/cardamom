933efb1da070805fb5b3200a213d1889bc6addae
ogandril <Olivier.Gandrillon@ens-lyon.fr>
2025-01-09 12:16:44 +0100

    K1D

--- Kanto_1D_OG.py
+++ Kanto_1D_OG.py
@@ -66,7 +66,7 @@ def multigene_kanto_1d(
 
     Returns:
         The (reduced) series of 1D Kantorovich distances.
-    """
+        
     assert u.ndim == 2 and v.ndim == 2, "expects 2D arrays"
     assert u.shape[1] == v.shape[1], "nb of distributions should be the same"
 
@@ -76,6 +76,20 @@ def multigene_kanto_1d(
 
     nb_genes = u.shape[1]
     return reduce(np.array([kanto_1d(u[:, i], v[:, i], p=p) for i in range(nb_genes)]))
+
+    assert u.ndim == 2 and v.ndim == 2, "expects 2D arrays"
+    assert u.shape[0] == v.shape[0], "nb of distributions should be the same"
+    """
+    nb_genes = u.shape[0]
+    res = np.zeros(nb_genes)
+    if normalization_max is not None:
+        for i in range(nb_genes):
+            if u[i, :].max() != 0:
+                u[i, :] *= normalization_max / u[i, :].max()
+            if v[i, :].max() != 0:
+                v[i, :] *= normalization_max / v[i, :].max()
+            res[i] = kanto_1d(u[i, :], v[i, :], p=p)
+    return res
 
 
 def main():
